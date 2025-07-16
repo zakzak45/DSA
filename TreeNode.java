c59c0310b7c2cfc601195b0f4d6ffe3efa5f175c
khunjuliwe <147825634+zakzak45@users.noreply.github.com>
2025-07-16 15:41:42 +0000

    TreeNode

---
+++ TreeNode.java
@@ -0,0 +1,15 @@
+public class TreeNode{
+ int value;
+ TreeNode left;
+ TreeNode right;
+
+
+ public void TreeNode(){
+    this.value =value;
+    this.left=null;
+    this.right=null;
+ }
+}
+
+//basic treenode in java
+            
