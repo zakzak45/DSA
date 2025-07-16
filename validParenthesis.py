4c3d0cec3c471a4ca0cb8b09c651237fcf1ac633
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-07-16 06:27:54 +0200

    added more algorihms

--- validParenthesis.py
+++ validParenthesis.py
@@ -1,1 +1,23 @@
-def validParenthesis(arr):
+def validParenthesis(s):
+    
+    stack =[]
+    mapping = {')': '(', '}': '{', ']': '['}
+    
+    
+    for char in s:
+         if char in mapping.values():
+             stack.append(char)
+         elif char in mapping:
+             if not stack or stack[-1] != mapping[char]:
+                 return False
+             stack.pop()
+         else:
+             return False
+         
+    return  not stack
+            
+    
+     
+     
+     
+     
