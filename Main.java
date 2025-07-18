838b9c5117aa589d676732326d468c3e71163606
khunjuliwe <147825634+zakzak45@users.noreply.github.com>
2025-07-18 18:58:49 +0000

    added linkedlist

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class Main{
+    public static void main(String [] args){
+       Node  node= new Node(10);
+       System.out.println("hello world");
+       System.out.println(node);
+
+
+       while(head!=null){
+        
+       }
+
+    }
+
+
+}
---
+++ Node.java
@@ -0,0 +1,20 @@
+public class Node{
+   int data;
+   Node next;
+ 
+  Node(int new_data){
+   this.data =new_data;
+   this.next =null;
+   }
+
+
+   
+}
+
+
+
+
+
+
+
+
